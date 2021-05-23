9dc98bfb63ca2f4ff0bfe06d43c23859f4e96a0a
工藤泰 <yutakakudo@kudouyasushinoMacBook-Pro.local>
2021-05-23 17:52:39 +0900

    scrape_tb以外コメントアウト

--- scrape/management/commands/scrape_run.py
+++ scrape/management/commands/scrape_run.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
 from scrape.scrape_tb import scrape_tb
-from scrape.scrape_google import scrape_google, scrape_google_get_storenames
-from scrape.scrape_uber import scrape_uber
-from scrape.scrape_hp import scrape_hp
-from scrape.scrape_gn import scrape_gn
+# from scrape.scrape_google import scrape_google, scrape_google_get_storenames
+# from scrape.scrape_uber import scrape_uber
+# from scrape.scrape_hp import scrape_hp
+# from scrape.scrape_gn import scrape_gn
 
 
 class Command(BaseCommand):  # コマンド python manage.py ~~
@@ -29,15 +29,15 @@ class Command(BaseCommand):  # コマンド python manage.py ~~
 
         if media == "tb":
             scrape_tb(area1, area2, range(start, end))
-        elif media == "hp":
-            scrape_hp(area1, area2, range(start, end))
-        elif media == "gn":
-            scrape_gn(area1, area2, range(start, end))
-        elif media == "google":
-            scrape_google(area1, area2, range(start, end))
-        elif media == "google_collect_store":
-            scrape_google_get_storenames(area1, area2, range(start, end))
-        elif media == "uber":
-            scrape_uber(area1, area2)
+        # elif media == "hp":
+        #     scrape_hp(area1, area2, range(start, end))
+        # elif media == "gn":
+        #     scrape_gn(area1, area2, range(start, end))
+        # elif media == "google":
+        #     scrape_google(area1, area2, range(start, end))
+        # elif media == "google_collect_store":
+        #     scrape_google_get_storenames(area1, area2, range(start, end))
+        # elif media == "uber":
+        #     scrape_uber(area1, area2)
 
             # return super().handle(*args, **options)
