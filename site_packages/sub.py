44f80c1c7177ba52ebc614d50854e2473be2a147
工藤泰 <yutakakudo@kudouyasushinoMacBook-Pro.local>
2021-07-30 19:15:09 +0900

    chain ok?

--- site_packages/sub.py
+++ site_packages/sub.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 import pykakasi
+import re
 
 from scrape import models
 
@@ -186,205 +187,206 @@ OTHER_THAN_RESTAURANTS = [
 def chain_replace(store_name: str) -> list:
 
     chain_dict = {
-        "新宿さぼてん": {
-            "short": "新宿さぼてん",
-            "long": "とんかつ新宿さぼてん"
-        },
-        "リンガーハット": {
-            "short": "リンガーハット",
-            "long": "長崎ちゃんぽんリンガーハット"
-        },
-        "CoCo壱番屋": {
-            "short": "CoCo壱番屋",
-            "long": "カレーハウスCoCo壱番屋"
-        },
-        "鳥貴族": {
-            "short": "鳥貴族",
-            "long": "焼鳥屋 鳥貴族"
-        },
-        "ちばチャン": {
-            "short": "ちばチャン",
-            "long": "大衆酒場 ちばチャン"
-        },
-        "大戸屋": {
-            "short": "大戸屋",
-            "long": "大戸屋 ごはん処"
-        },
-        "京ほのか": {
-            "short": "京ほのか",
-            "long": "完全個室 居酒屋 京ほのか"
-        },
-        "五右衛門": {
-            "short": "五右衛門",
-            "long": "洋麺屋五右衛門"
-        },
-        "ダンダダン酒場": {
-            "short": "ダンダダン酒場",
-            "long": "肉汁餃子製作所 ダンダダン酒場"
-        },
-        "ミライザカ": {
-            "short": "ミライザカ",
-            "long": "旨唐揚げと居酒メシ ミライザカ"
-        },
-        "すたみな太郎NEXT": {
-            "short": "すたみな太郎NEXT",
-            "long": "食べ放題・バイキング すたみな太郎NEXT"
-        },
-        "くら寿司": {
-            "short": "くら寿司",
-            "long": "無添くら寿司"
-        },
-        "つぼ八": {
-            "short": "つぼ八",
-            "long": "北海道の恵み つぼ八"
-        },
-        "フレッシュネスバーガー": {
-            "short": "フレッシュネスバーガー",
-            "long": "FRESHNESS BURGER（フレッシュネスバーガー）"
-        },
-        "千年の宴": {
-            "short": "千年の宴",
-            "long": "個室空間 湯葉豆腐料理 千年の宴"
-        },
-        "はなの舞": {
-            "short": "はなの舞",
-            "long": "海鮮居酒屋 はなの舞"
-        },
-        "とりいちず": {
-            "short": "とりいちず",
-            "long": "水炊き・焼鳥・鶏餃子 とりいちず"
-        },
-        "酔っ手羽": {
-            "short": "酔っ手羽",
-            "long": "居酒屋革命酔っ手羽"
-        },
-        "博多劇場": {
-            "short": "博多劇場",
-            "long": "屋台屋 博多劇場"
-        },
-        "甘太郎": {
-            "short": "甘太郎",
-            "long": "手作り居酒屋 甘太郎"
-        },
-        "四六時中": {
-            "short": "四六時中",
-            "long": "おひつごはん四六時中"
-        },
-        "すた丼": {
-            "short": "元祖すた丼の店",
-            "long": "伝説のすた丼屋"
-        },
-        "ポポラマーマ": {
-            "short": "ポポラマーマ",
-            "long": "ゆであげ生パスタ ポポラマーマ"
-        },
-        "ブロンコビリー": {
-            "short": "ブロンコビリー",
-            "long": "ステーキハウス ブロンコビリー"
-        },
-        "HUB": {
-            "short": "HUB",
-            "long": "BRITISH PUB HUB"
-        },
-        "牛角": {
-            "short": "牛角",
-            "long": "炭火焼肉酒家 牛角"
-        },
-        "忍家": {
-            "short": "全席個室居酒屋 忍家",
-            "long": "全席個室ダイニング 忍家"
-        },
-        "木曽路": {
-            "short": "木曽路",
-            "long": "しゃぶしゃぶ 日本料理 木曽路"
-        },
-        "さくら水産": {
-            "short": "さくら水産",
-            "long": "海産物居酒屋 さくら水産"
-        },
-        "楽蔵": {
-            "short": "楽蔵",
-            "long": "全席個室 楽蔵"
-        },
-        "北海道": {
-            "short": "北海道",
-            "long": "北の味紀行と地酒 北海道"
-        },
-        "ありがとう": {
-            "short": "ありがとう",
-            "long": "みんなの居酒屋ありがとう"
-        },
-        "ダンダダン": {
-            "short": "ダンダダン酒場",
-            "long": "肉汁餃子のダンダダン"
-        },
-        "ディプント": {
-            "short": "ワインの酒場 ディプント",
-            "long": "ワインの酒場。ディプント"
-        },
-        "銀座ライオン": {
-            "short": "銀座ライオン",
-            "long": "ビアホール銀座ライオン"
-        },
-        "とんでん": {
-            "short": "和食レストランとんでん",
-            "long": "北海道生まれ和食処とんでん"
-        },
-        "扇屋": {
-            "short": "扇屋",
-            "long": "やきとりの扇屋"
-        },
-        "いきなり": {
-            "short": "いきなりステーキ",
-            "long": "いきなり！ステーキ"
-        },
-        "コメダ珈琲店": {
-            "short": "コメダ珈琲店",
-            "long": "珈琲所 コメダ珈琲店"
-        },
-        "温野菜": {
-            "short": "温野菜",
-            "long": "しゃぶしゃぶ温野菜"
-        },
-        # "": {
-        #     "short": "",
-        #     "long": ""
-        # },
-        # "": {
-        #     "short": "",
-        #     "long": ""
-        # },
-        # "": {
-        #     "short": "",
-        #     "long": ""
-        # },
+        "新宿さぼてん": [
+            "新宿さぼてん",
+            "とんかつ新宿さぼてん",
+        ],
+        "リンガーハット": [
+            "リンガーハット",
+            "長崎ちゃんぽんリンガーハット",
+        ],
+        "CoCo壱番屋": [
+            "CoCo壱番屋",
+            "カレーハウスCoCo壱番屋",
+        ],
+        "鳥貴族": [
+            "鳥貴族",
+            "焼鳥屋 鳥貴族",
+        ],
+        "ちばチャン": [
+            "ちばチャン",
+            "大衆酒場 ちばチャン",
+        ],
+        "大戸屋": [
+            "大戸屋",
+            "大戸屋 ごはん処",
+        ],
+        "京ほのか": [
+            "京ほのか",
+            "完全個室 居酒屋 京ほのか",
+        ],
+        "五右衛門": [
+            "五右衛門",
+            "洋麺屋五右衛門",
+        ],
+        "ダンダダン酒場": [
+            "ダンダダン酒場",
+            "肉汁餃子製作所 ダンダダン酒場",
+        ],
+        "ミライザカ": [
+            "ミライザカ",
+            "旨唐揚げと居酒メシ ミライザカ",
+        ],
+        "すたみな太郎NEXT": [
+            "すたみな太郎NEXT",
+            "食べ放題・バイキング すたみな太郎NEXT",
+        ],
+        "くら寿司": [
+            "くら寿司",
+            "無添くら寿司",
+        ],
+        "つぼ八": [
+            "つぼ八",
+            "北海道の恵み つぼ八",
+        ],
+        "フレッシュネスバーガー": [
+            "フレッシュネスバーガー",
+            "FRESHNESS BURGER（フレッシュネスバーガー）",
+        ],
+        "千年の宴": [
+            "千年の宴",
+            "個室空間 湯葉豆腐料理 千年の宴",
+        ],
+        "はなの舞": [
+            "はなの舞",
+            "海鮮居酒屋 はなの舞",
+        ],
+        "とりいちず": [
+            "とりいちず",
+            "水炊き・焼鳥・鶏餃子 とりいちず",
+        ],
+        "酔っ手羽": [
+            "酔っ手羽",
+            "居酒屋革命酔っ手羽",
+        ],
+        "博多劇場": [
+            "博多劇場",
+            "屋台屋 博多劇場",
+        ],
+        "甘太郎": [
+            "甘太郎",
+            "手作り居酒屋 甘太郎",
+        ],
+        "四六時中": [
+            "四六時中",
+            "おひつごはん四六時中",
+        ],
+        "すた丼": [
+            "元祖すた丼の店",
+            "伝説のすた丼屋",
+        ],
+        "ポポラマーマ": [
+            "ポポラマーマ",
+            "ゆであげ生パスタ ポポラマーマ",
+        ],
+        "ブロンコビリー": [
+            "ブロンコビリー",
+            "ステーキハウス ブロンコビリー",
+        ],
+        "HUB": [
+            "HUB",
+            "BRITISH PUB HUB",
+        ],
+        "牛角": [
+            "牛角",
+            "炭火焼肉酒家 牛角",
+        ],
+        "忍家": [
+            "全席個室居酒屋 忍家",
+            "全席個室ダイニング 忍家",
+        ],
+        "木曽路": [
+            "木曽路",
+            "しゃぶしゃぶ 日本料理 木曽路",
+        ],
+        "さくら水産": [
+            "さくら水産",
+            "海産物居酒屋 さくら水産",
+        ],
+        "楽蔵": [
+            "楽蔵",
+            "全席個室 楽蔵",
+        ],
+        "北海道": [
+            "北海道",
+            "北の味紀行と地酒 北海道",
+        ],
+        "ありがとう": [
+            "ありがとう",
+            "みんなの居酒屋ありがとう",
+        ],
+        "ダンダダン": [
+            "ダンダダン酒場",
+            "肉汁餃子のダンダダン",
+        ],
+        "ディプント": [
+            "ワインの酒場 ディプント",
+            "ワインの酒場。ディプント",
+        ],
+        "銀座ライオン": [
+            "銀座ライオン",
+            "ビアホール銀座ライオン",
+        ],
+        "とんでん": [
+            "和食レストランとんでん",
+            "北海道生まれ和食処とんでん",
+        ],
+        "扇屋": [
+            "扇屋",
+            "やきとりの扇屋",
+        ],
+        "いきなり": [
+            "いきなりステーキ",
+            "いきなり！ステーキ",
+        ],
+        "コメダ珈琲店": [
+            "コメダ珈琲店",
+            "珈琲所 コメダ珈琲店",
+        ],
+        "温野菜": [
+            "温野菜",
+            "しゃぶしゃぶ温野菜",
+        ],
+        "山田うどん": [
+            "山田うどん",
+            "山田うどん食堂",
+            "ファミリー食堂 山田うどん食堂",
+        ],
+        "カフェベローチェ": [
+            "カフェベローチェ",
+            "カフェ・ベローチェ",
+        ],
+        "": [
+            "",
+            "",
+        ],
+        "": [
+            "",
+            "",
+        ],
+        "": [
+            "",
+            "",
+        ],
+        "": [
+            "",
+            "",
+        ],
     }
 
-    import re
-        # いきなりステーキ 松戸店
-    chain_dict = {"いきなり": [
-        "いきなり",
-        "いきなり  ステーキ",
-        "いきなり！ステーキ",
-        "魁いきなりステーキ",
-        ]}
-
-    store_name = "いきなりステー  キ松戸市".replace(' ', '')
-    # re.match("いきなり",store_name).group()
-
+    chain_dict = {k:v for k,v in chain_dict.items() if k} # 空除去
     store_name = store_name.replace(' ', '')
     # chain_dict keyを回して該当するかチェック
     key = [s for s in chain_dict.keys() if s.replace(' ', '') in store_name]
     if len(key) == 1:
         key = key[0]  # リスト解除
-        kouho_list:list = chain_dict[key]
+        kouho_list: list = chain_dict[key]
         kouho_list = [name.replace(' ', '') for name in kouho_list]
-        try: 
-            hit_sentence = max([re.match(s,store_name).group() for s in kouho_list if re.match(s,store_name)])
-            kouho_list.remove(hit_sentence) # あるやつは除外してメモリ節約
+        try:
+            hit_sentence = max([re.match(s, store_name).group() for s in kouho_list if re.match(s, store_name)])
+            kouho_list.remove(hit_sentence)  # あるやつは除外してメモリ節約
 
-            
-            return [store_name.replace(hit_sentence,kouho) for kouho in kouho_list]
+            return [store_name.replace(hit_sentence, kouho) for kouho in kouho_list]
 
         except Exception:
             return []
@@ -420,17 +422,17 @@ def category_set(st_obj, category_list, errorlist=None):
 
 
 def name_set(st_obj: models.Store, store_name: str, mt_str: str, yomigana: str = "", yomi_roma: str = ""):
-    
-    if getattr(st_obj,f"store_name_{mt_str}") != store_name:
+
+    if getattr(st_obj, f"store_name_{mt_str}") != store_name:
         st_obj.update_name(store_name, mt_str)
 
     if mt_str == "tb" and (yomigana or yomi_roma):  # 読み仮名をもっている自信のある食べログなら正式名称にする
-        if getattr(st_obj,"store_name") != store_name:
+        if getattr(st_obj, "store_name") != store_name:
             st_obj.update_name(store_name)
 
     # よみがなーーーーーーーーーーー
     if (yomigana and mt_str == "tb") or (yomigana and mt_str == "gn" and not st_obj.yomigana):
-        if getattr(st_obj,"yomigana") != yomigana:
+        if getattr(st_obj, "yomigana") != yomigana:
             st_obj.yomigana = yomigana
             st_obj.save()
     elif (mt_str == "google" and not st_obj.yomigana):
@@ -441,7 +443,7 @@ def name_set(st_obj: models.Store, store_name: str, mt_str: str, yomigana: str =
 
     # よみローマーーーーーーーーーーーー
     if (yomi_roma and mt_str == "tb") or (yomi_roma and mt_str == "gn" and not st_obj.yomi_roma):
-        if getattr(st_obj,"yomi_roma") != yomi_roma:
+        if getattr(st_obj, "yomi_roma") != yomi_roma:
             st_obj.yomi_roma = yomi_roma
             st_obj.save()
     elif (mt_str == "google" and not st_obj.yomi_roma):
@@ -453,7 +455,7 @@ def name_set(st_obj: models.Store, store_name: str, mt_str: str, yomigana: str =
 
 def address_set(st_obj, address, mt_str):
     if (address and not st_obj.address) or (address and mt_str == "gn") or (address and mt_str == "tb"):
-        if getattr(st_obj,"address") != address:
+        if getattr(st_obj, "address") != address:
             st_obj.address = address
             st_obj.save()
 
